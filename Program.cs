e01fab278a75c30c518bb3a2f7b6ac7482683d60
Juliolimahen <juliolima_henrique@outlook.com>
2023-08-04 10:40:38 -0300

    add git ignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using exemplo_injecao_de_dependencia.Interface;
+using exemplo_injecao_de_dependencia.Model;
+using exemplo_injecao_de_dependencia.Services;
+
+namespace exemplo_injecao_de_dependencia;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        ILogger logger = new FileLogger("log.txt");
+        Logger myLogger = new Logger(logger);
+
+        myLogger.LogMessage("Mensagem de log importante!");
+    }
+
+}
